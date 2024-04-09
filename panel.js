e38f745aea90eec93d256777f8dece037dc93177
Vincent55 <5020559@gmail.com>
2024-04-10 02:57:47 +0800

    Update filter feature

--- panel.js
+++ panel.js
@@ -1,4 +1,19 @@
+var filter = "";
+// 獲取輸入欄位和顯示值的元素
+const inputField = document.getElementById('inputField');
+const displayValue = document.getElementById('displayValue');
+
+inputField.addEventListener('input', function(event) {
+    filter = event.target.value;
+    displayValue.textContent = filter;
+});
+
 
 chrome.devtools.network.onRequestFinished.addListener((request) => {
-  console.log(request);
+  request.getContent((content) => {
+    console.log(filter);
+    if (content && content.includes(filter)) {
+      console.log(content);
+    }
+  });
 });
