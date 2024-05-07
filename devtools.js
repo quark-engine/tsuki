b99520d1d546d274d0ef265a97292e0c821f5730
Vincent550102 <5020559@gmail.com>
2024-05-07 22:54:39 +0800

    Rename to AutoTsuki

--- devtools.js
+++ devtools.js
@@ -3,7 +3,7 @@ const defaultTemplate = {
   "Normal Filter(content)": "if(content.includes('NEEDCHANGE')) {\n  console.log(url);\n}"
 }
 
-chrome.devtools.panels.create('demo panel', 'icon.png', 'panel.html', () => {
+chrome.devtools.panels.create('AutoTsuki', 'icon.png', 'panel.html', () => {
   console.log('user switched to this panel');
   chrome.storage.local.clear(()=>{
     console.log('Cleared storage');
